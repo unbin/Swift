cff6d0785a17a5a97ae72eaf5b7c027784c0dac6
Daniel Skybin <unbin1234@gmail.com>
2018-08-16 21:41:03 -0400

    Upgraded Cel to Far converter

--- projects/Celcius to Farenheiht converter/Celcius to Farenheiht converter/ViewController.swift
+++ projects/Celcius to Farenheiht converter/Celcius to Farenheiht converter/ViewController.swift
@@ -23,18 +23,35 @@ class ViewController: UIViewController {
     @IBOutlet weak var celtext: UITextField!
     @IBOutlet weak var fartext: UITextField!
     
-    func convert(C:Float) -> Float{
+    func convertCtoF(C:Float) -> Float{
         var F:Float
         F = C * 1.8 + 32
         return F
     }
     
-    @IBAction func press(_ sender: Any) {
-        if celtext.text != nil {
-            let C = Float(celtext.text!)
-            fartext.text = String(convert(C:C!))
+    func convertFtoC(F:Float) -> Float{
+        var C:Float
+        C = (F - 32) / 1.8
+        return C
+    }
+    
+    @IBAction func celbox(_ sender: Any) {
+        if celtext.text != ""{
+            let C:Float = Float(celtext.text!)!
+            fartext.text = String(convertCtoF(C:C))
+        } else {
+            celtext.text = String(0.0)
         }
     }
-
+    
+    @IBAction func farbox(_ sender: Any) {
+        if fartext.text != ""{
+            let F:Float = Float(fartext.text!)!
+            celtext.text = String(convertFtoC(F:F))
+        } else {
+            fartext.text = String(0.0)
+        }
+    }
 }
+
 
